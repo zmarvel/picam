2aaad6d4a389e31d214c103edaff6a00b8f95d14
Zack Marvel <zpmarvel@gmail.com>
2019-11-04 01:10:23 -0700

    Parse basic info from image messages
    Also added a `data` field to the Image message.
    
    Tested that we're receiving messages from the Pi as expected and we're
    pulling out the size and time correctly.

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -51,6 +51,8 @@ func handleConnection(conn net.Conn) {
 	log.Printf("Connection with %v established\n", conn.RemoteAddr())
 	done := false
 	for !done {
+		// Read the size first so we know how big the rest of the message is.
+		// This is not a member of the protobuf--it gets sent just before.
 		var size int32 = 0
 		err := binary.Read(conn, binary.BigEndian, &size)
 		if err != nil {
@@ -59,19 +61,24 @@ func handleConnection(conn net.Conn) {
 			break
 		}
 
+		// Now we know how big of a buffer we need
 		var imageMessage picam.Image
 		buffer := make([]uint8, size)
 		for read := 0; read < int(size); {
-			nRead, err := conn.Read(buffer[read:])
+			chunkSize, err := conn.Read(buffer[read:])
 			if err != nil {
 				// We encountered an error--exit the goroutine
 				log.Printf("Connection with %v dropped\n", conn.RemoteAddr())
 				done = true
 				break
 			}
-			read += nRead
+			read += chunkSize
 		}
 		proto.Unmarshal(buffer, &imageMessage)
 		fmt.Printf("Read Image (%v bytes)\n", size)
+		// Image is only valid with metadata
+		if meta := imageMessage.Metadata; meta != nil {
+			fmt.Printf("Image time (%v, %v)\n", meta.TimeS, meta.TimeUs)
+		}
 	}
 }
