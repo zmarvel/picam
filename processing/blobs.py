13162f2ef75ed2acbf4cf3b37ce0d5491fe26e68
Zack Marvel <zpmarvel@gmail.com>
2019-11-10 01:18:30 -0700

    Flood fill stars

--- processing/blobs.py
+++ processing/blobs.py
@@ -12,36 +12,67 @@ import matplotlib.pyplot as plt
 import scipy.signal as spsignal
 
 parser = ap.ArgumentParser()
-parser.add_argument('input_file', nargs='?', default='./out/469_bright_region.png')
+parser.add_argument('input_file', nargs='?', default='./out/469_smaller_cropped.png')
 parser.add_argument('-c', '--color-map', default='gray')
 
 args = parser.parse_args()
 
 unprocessed_rgb = np.array(Image.open(args.input_file))
 
-unprocessed_rgb = np.delete(unprocessed_rgb, 3, 2) 
-unprocessed = unprocessed_rgb.sum(axis=2, dtype=np.float)
+if unprocessed_rgb.shape[2] == 2:
+    # Grayscale image with alpha
+    unprocessed = np.delete(unprocessed_rgb, 1, 2).squeeze()
+elif unprocessed_rgb.shape[2] == 4:
+    # RGBA image
+    unprocessed_rgb = np.delete(unprocessed_rgb, 3, 2) 
+    unprocessed = unprocessed_rgb.sum(axis=2, dtype=np.float)
 
-ax = plt.subplot(1, 2, 1)
-ax.imshow(unprocessed, cmap=args.color_map)
+threshold_lo = 50
+unprocessed = np.ma.array(unprocessed, mask=unprocessed < threshold_lo).filled(0)
 
-mu, sigma = (0, 5)
-var = sigma**2
 
-kernel_size = 5
+def is_in_bounds(mat, row, col):
+    return row > 0 and row < mat.shape[0] - 1 and col > 0 and col < mat.shape[1] - 1
 
-xs = np.arange(-kernel_size // 2, kernel_size // 2)
+NEIGHBORS = (
+    (-1, 0),
+    (1, 0),
+    (0, -1),
+    (0, 1),
+    (-1, -1),
+    (-1, 1),
+    (1, 1),
+    (1, -1),
+)
+def flood_fill(fill_color, mat, out, start):
+    q = [start]
+    while len(q) > 0:
+        row, col = q.pop()
+        if mat[row][col] > 0:
+            out[row][col] = fill_color
 
-ys = 1 / sqrt(2 * pi * var) * np.exp(-(xs - mu)**2 / (2*var))
+        for dr, dc in zip(range(-2, 2), range(-2, 2)):
+        #for dr, dc in NEIGHBORS:
+            r, c = (row+dr, col+dc)
+            if is_in_bounds(mat, r, c) and out[r][c] == 0 and mat[r][c] > 0:
+                q.append((r, c))
 
-kernel = np.zeros((kernel_size, kernel_size))
-kernel[:,kernel_size // 2] = ys
-kernel[kernel_size // 2,:] = ys
+shape = unprocessed.shape
+flood = np.zeros(shape)
+fill_color = 1
+for r in range(shape[0]):
+    for c in range(shape[1]):
+        if unprocessed[r][c] > 0 and flood[r][c] == 0:
+            flood_fill(fill_color, unprocessed, flood, (r, c))
+            fill_color += 1
 
-#for x in xs:
-#    print((1 / sqrt(2 * pi * var)) * exp(-(x - mu)**2 / (2 * var)))
-blurred = spsignal.convolve2d(unprocessed, kernel)
-ax = plt.subplot(1, 2, 2)
-ax.imshow(blurred, cmap=args.color_map)
+print(f'Identified {fill_color} regions')
+
+ax = plt.subplot(1, 1, 1)
+ax.imshow(flood, cmap='gist_ncar')
+
+#blurred = spsignal.convolve2d(unprocessed, kernel)
+#ax = plt.subplot(1, 2, 2)
+#ax.imshow(blurred, cmap=args.color_map)
 
 plt.show()
