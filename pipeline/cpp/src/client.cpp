5c17ef701ca65a3c0c12d1bb199d809a1db317fb
Zack Marvel <zpmarvel@gmail.com>
2020-04-24 00:23:42 -0600

    sending and receiving iamge data over zeromq

--- pipeline/cpp/src/client.cpp
+++ pipeline/cpp/src/client.cpp
@@ -1,2 +1,22 @@
 
 
+#include "zmqpp/zmqpp.hpp"
+
+int main(int argc, char *argv[]) {
+  zmqpp::context context{};
+
+  zmqpp::socket subscriber{context, zmqpp::socket_type::subscribe};
+  subscriber.connect("tcp://localhost:9000");
+  subscriber.subscribe("PNG ");
+
+  while (true) {
+    zmqpp::message rcvd;
+    subscriber.receive(rcvd);
+    std::string filter, data;
+    rcvd >> filter;
+    rcvd >> data;
+    std::cout << "got " << rcvd.parts() << " parts: " <<
+      filter.size() << "/" << data.size() << std::endl;
+    std::cout << filter << std::endl;
+  }
+}
--- pipeline/cpp/src/server.cpp
+++ pipeline/cpp/src/server.cpp
@@ -1,6 +1,10 @@
 
+#include <memory>
+
+#include <unistd.h>
 #include <png++/png.hpp>
 #include "zmqpp/zmqpp.hpp"
+
 
 /**
  * This program expects a number in milliseconds, denoting a rate at which
@@ -8,13 +12,53 @@
  */
 
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << "USAGE: " << argv[0] << " rate_ms" << std::endl;
+    return -1;
+  }
+
+  const auto rate_ms = std::atoi(argv[1]);
+  if (rate_ms == 0) {
+    std::cerr << "Could not convert argument to int" << std::endl;
+    std::cout << "USAGE: " << argv[0] << " rate_ms" << std::endl;
+    return -1;
+  }
+
   png::image<png::rgb_pixel> image{"344.png"};
-  uint32_t width = image.get_width();
-  uint32_t height = image.get_height();
+  const uint32_t width = image.get_width();
+  const uint32_t height = image.get_height();
+
+  std::cout << "width: " << width << std::endl
+    << "height: " << height << std::endl;
 
   zmqpp::context context{};
-  zmqpp::socket publisher{context, zqmpp::socket_type::publish};
+  zmqpp::socket publisher{context, zmqpp::socket_type::publish};
   publisher.bind("tcp://*:9000");
 
+  // Only worry about RGB (drop the A)
+  std::vector<uint8_t> pixbuf{};
+  pixbuf.reserve(width * height * 3);
+
+  for (uint32_t y = 0; y < height; y++) {
+    for (uint32_t x = 0; x < width; x++) {
+      uint32_t i = y * width + x;
+      pixbuf.push_back(image[y][x].red);
+      pixbuf.push_back(image[y][x].blue);
+      pixbuf.push_back(image[y][x].green);
+    }
+  }
+
+  std::cout << "pixbuf size: " << pixbuf.size() << std::endl;
+
+  // zmqpp::message image_msg{};
+  // image_msg << pixbuf.data();
+
+  for (int i = 0; i < 300; i++) {
+    zmqpp::message image_msg{};
+    image_msg << "PNG ";
+    image_msg.add_raw(pixbuf.data(), pixbuf.size());
+    publisher.send(image_msg);
+    usleep(rate_ms * 1000);
+  }
 }
 
