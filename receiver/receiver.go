522608eea6402ec5a7045eb7f6111bd1bb379ef7
Zack Marvel <zpmarvel@gmail.com>
2020-05-12 00:06:01 -0600

    receiver: groundwork for monitoring size of image log
    Next I need to decide how often to check--does it get really expensive
    if we do it every time we get an image? If there are multiple image
    sources, is it possible it could result in unnecessary IO? Does it even
    matter right now?

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -3,14 +3,19 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"github.com/BurntSushi/toml"
 	"github.com/golang/protobuf/proto"
+	"encoding/json"
 	"log"
 	"net"
 	"os"
 	"time"
 	"path"
 	"picam"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 const (
@@ -32,12 +37,58 @@ type Config struct {
 
 	Log struct {
 		Path string
+		MaxSize string
 	}
 }
 
-func (config *Config) DBString() string {
-	return fmt.Sprintf("dbname=%v user=%v password=%v host=%v sslmode=disable",
-		config.DB.Database, config.DB.User, config.DB.Password, config.DB.Host)
+// Translate the max_size field in the config into a size in bytes
+func (config *Config) ParseMaxSize() (uint64, error) {
+	spacesRemoved := strings.Replace(config.Log.MaxSize, " ", "", 0)
+	var sizeBuilder strings.Builder
+	var unitBuilder strings.Builder
+	for _, r := range spacesRemoved {
+		if unitBuilder.Len() == 0 && unicode.IsNumber(r) {
+			_, err := sizeBuilder.WriteRune(r)
+			if err != nil {
+				return 0, fmt.Errorf("ParseMaxSize: %v", err)
+			}
+		} else if unitBuilder.Len() > 0 && unicode.IsLetter(r) {
+			_, err := unitBuilder.WriteRune(r)
+			if err != nil {
+				return 0, fmt.Errorf("ParseMaxSize: %v", err)
+			}
+		} else {
+			return 0, fmt.Errorf("ParseMaxSize: Unrecognized rune %v", r)
+		}
+	}
+
+	if unitBuilder.Len() == 0 {
+		_, err := unitBuilder.WriteString("k")
+		if err != nil {
+			return 0, fmt.Errorf("ParseMaxSize: %v", err)
+		}
+	}
+
+	sizeStr := sizeBuilder.String()
+	unitStr := strings.ToLower(unitBuilder.String())
+
+	sizeNum, err := strconv.Atoi(sizeStr)
+	if err != nil {
+		return 0, fmt.Errorf("ParseMaxSize: Unable to convert size to number: %v",
+			err)
+	}
+
+	if unitStr == "k" {
+		sizeNum *= 1024
+	} else if unitStr == "m" {
+		sizeNum *= 1024 * 1024
+	} else if unitStr == "g" {
+		sizeNum *= 1024 * 1024 * 1024
+	} else {
+		return 0, fmt.Errorf("ParseMaxSize: Unrecognized unit %s", unitStr)
+	}
+
+	return uint64(sizeNum), nil
 }
 
 func main() {
@@ -95,23 +146,15 @@ func handleConnection(logDir string, conn net.Conn) {
 			}
 			read += chunkSize
 		}
+
 		proto.Unmarshal(buffer, &imageMessage)
 		fmt.Printf("Read Image (%v bytes)\n", size)
-		// Image is only valid with metadata
-		meta := imageMessage.Metadata
-		if meta != nil {
-			fmt.Printf("Image time (%v, %v)\n", meta.TimeS, meta.TimeUs)
-		}
 
-		if imageData := imageMessage.Data; imageData != nil {
-			if err != nil {
-				logFile, err := os.Create(path.Join(logDir, fmt.Sprintf("%d.%06d.fits",
-						meta.TimeS, meta.TimeUs)))
-				defer logFile.Close()
-				if err != nil {
-				}
-			}
-
+		imagePath, err := logImage(logDir, &imageMessage)
+		if err != nil {
+			log.Printf("Failed to log image: %v", err)
+		} else {
+			log.Printf("Logged image to %s", imagePath)
 		}
 	}
 	log.Printf("Connection with %v closed\n", conn.RemoteAddr())
@@ -119,40 +162,57 @@ func handleConnection(logDir string, conn net.Conn) {
 
 func logImage(logDir string, image *picam.Image) (string, error) {
 	meta := image.Metadata
+	if (meta == nil || image.Data == nil) {
+		return "", fmt.Errorf("logImage: message contains nil")
+	}
+
 	timestamp := time.Unix(meta.TimeS, 1000 * meta.TimeUs)
 	subdir := path.Join(formatDayDir(timestamp))
 
 	// Check if the day's directory already exists. If it does exist, make sure
 	// it's a directory. Otherwise, if it doesn't exist, create it.
 	subdirInfo, err := os.Stat(subdir)
-	if (err == nil && !subdirInfo.IsDir()) {
+	if err == nil && !subdirInfo.IsDir() {
 		// The file exists but isn't a directory--we can't proceed
 		return "", fmt.Errorf("logImage: %s exists but is not a directory", subdir)
 	} else if (os.IsExist(err)) {
 		// The directory doesn't exist--create it!
 		err := os.Mkdir(subdir, 0755)
-		if (err != nil) {
+		if err != nil {
 			return "", fmt.Errorf("logImage: failed to create directory at %s", subdir)
 		}
 	}
 
 	// Log the image and its metadata in the day's directory.
 	imgFilename := path.Join(subdir, formatImageFilename(timestamp))
-	// imgFile, err := os.Create(imgFilename)
-	_, err = os.Create(imgFilename)
-	if (err != nil) {
+	imgFile, err := os.Create(imgFilename)
+	defer imgFile.Close()
+	if err != nil {
 		return "", fmt.Errorf("logImage: failed to create image file at %s",
 			imgFilename)
 	}
 	metaFilename := path.Join(subdir, formatMetaFilename(timestamp))
-	// metaFile, err := os.Create(metaFilename)
-	_, err = os.Create(metaFilename)
-	if (err != nil) {
+	metaFile, err := os.Create(metaFilename)
+	defer metaFile.Close()
+	if err != nil {
 		return "", fmt.Errorf("logImage: failed to create metadata file at %s",
 			metaFilename)
 	}
 
-	
+	metaJson, err := json.Marshal(meta)
+	if err != nil {
+		return "", fmt.Errorf("logImage: failed to serialize metadata")
+	}
+
+	_, err = metaFile.Write(metaJson)
+	if err != nil {
+		return "", fmt.Errorf("logImage: failed to write to metadata file")
+	}
+
+	_, err = imgFile.Write(image.Data)
+	if err != nil {
+		return "", fmt.Errorf("logImage: failed to write image data to file")
+	}
 
 	return imgFilename, nil
 }
@@ -167,4 +227,40 @@ func formatImageFilename(tm time.Time) string {
 
 func formatMetaFilename(tm time.Time) string {
 	return tm.Format("150405.000.json")
+}
+
+// Recursively calculate the space occupied by a directory
+func DiskUsage(dirPath string) (int64, error) {
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		return 0, fmt.Errorf("DiskUsage: Failed to open dir %s: %v", dirPath, err)
+	}
+
+	infos, err := dir.Readdir(0)
+	if err == io.EOF {
+		dirInfo, err := dir.Stat()
+		if err != nil {
+			return 0, fmt.Errorf("DiskUsage: Failed to stat directory: %v", err)
+		}
+
+		var sum int64 = dirInfo.Size()
+		for _, info := range infos {
+			sum += info.Size()
+			// If the file is a subdirectory, traverse it recursively
+			if info.IsDir() {
+				childPath := path.Join(dirPath, info.Name())
+				childSum, err := DiskUsage(childPath)
+				if (err != nil) {
+					return 0, fmt.Errorf("DiskUsage: Failed to get size of child %s: %v",
+						childPath, err)
+				}
+				sum += childSum
+			}
+		}
+		return sum, nil
+	} else if err != nil {
+		return 0, fmt.Errorf("DiskUsage: Failed to scan directory: %v", err)
+	} else {
+		return 0, fmt.Errorf("DiskUsage: EOF expected!")
+	}
 }
